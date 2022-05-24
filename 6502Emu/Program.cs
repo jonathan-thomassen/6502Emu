90f318bfbe4bce4083a3eb1ea3ec967df2d5ff96
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2022-05-24 12:59:54 +0200

    Look for 6502 binary in bin folder

--- 6502Emu/Program.cs
+++ 6502Emu/Program.cs
@@ -1,6 +1,6 @@
 ﻿using static _6502Emu.Memory;
 using static _6502Emu.CPU;
 
-LoadROMFile(@"C:\Users\jonat\Desktop\o6502-2022-05-08-194137.bin");
+LoadROMFile(@"6502test.bin");
 WriteByte(RESVectorHigh, 0x08);
 Reset();
